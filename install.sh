c6afda51c56589986fedc73e86334adb5546c30a
root <root@lab-test01.kazutan.info>
2014-07-13 04:45:19 +0900

    first commit

--- install.sh
+++ install.sh
@@ -4,3 +4,19 @@ yum install -y httpd mysql mysql-server php php-mysql
 rpm -ivh http://repo.zabbix.com/zabbix/2.2/rhel/6/x86_64/zabbix-release-2.2-1.el6.noarch.rpm
 yum install -y zabbix-server-mysql zabbix-web-mysql
 yum install -y zabbix-agent
+
+
+echo << EOF > /etc/my.cnf
+[mysqld]
+datadir=/var/lib/mysql
+socket=/var/lib/mysql/mysql.sock
+user=mysql
+# Disabling symbolic-links is recommended to prevent assorted security risks
+symbolic-links=0
+default-character-set=utf8
+
+[mysqld_safe]
+log-error=/var/log/mysqld.log
+pid-file=/var/run/mysqld/mysqld.pid
+
+EOF
